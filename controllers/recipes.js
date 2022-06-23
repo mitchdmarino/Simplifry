f1431cc24e1ff49fd736ee38280526cdb4aef051
mitchdmarino <mitchdmarino@gmail.com>
2022-06-23 00:46:37 -0700

    finishes recipe and ingredient creation, reading, and updating

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,9 +4,6 @@ const router = express.Router()
 const db = require('../models')
 
 
-
-// routes 
-
 // GET /recipes, show a list of the user's recipes (if they are logged in )
 router.get('/', async (req,res) => {
     if (!res.locals.user) {
@@ -19,31 +16,12 @@ router.get('/', async (req,res) => {
     }
 })
 
-
-
 // GET /recipes/new to show form that will create a new recipe for the user 
-// also need to create new ingredients associated with the recipe 
-
-// GET /recipes/new for user to create a new recipe (associated with their account)
 router.get('/new', (req,res) => {
     res.render('recipes/new')
 })
 
-// GET /ingredients/new 
-
-router.get('/edit/:id', async (req,res) => {
-    const recipe = await db.recipe.findOne({
-        where: {
-            id: req.params.id
-        }
-    })
-    // show an edit form for the recipe
-    res.render('recipes/edit', {recipe: recipe})
-})
-
 // POST route for submission of create recipe form 
-// need to be able to create ingredients that will be associated with recipe id 
-
 router.post('/', async (req,res) => {
     try {
         const user = res.locals.user 
@@ -54,18 +32,73 @@ router.post('/', async (req,res) => {
             notes: req.body.notes,
             userId: user.id
         })
-        await recipe.createIngredient({
-            name: req.body.ingredientName,
-            measure: req.body.ingredientMeasure,
-            quantity: req.body.ingredientQuantity,
-            recipeId: Number(req.body.ingredientRecipeId)
-        })
-        console.log(recipe)
         let recipeID = await recipe.id
-        console.log(recipeID)
-        res.redirect(`/${recipeID}`)
-        
-        
+        res.redirect(`/recipes/${recipeID}/ingredients/new`)   
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+// POST /recipes/ingredients to create and add ingredients to the recipe 
+router.post('/ingredients', async (req,res) => {
+    console.log()
+    const newIngredient = await db.ingredient.create({
+        name: req.body.ingrName, 
+        measure: req.body.ingrMeasure,
+        quantity: req.body.ingrQuantity, 
+        recipeId: Number(req.body.recipeId)
+    })
+    console.log(newIngredient)
+    res.redirect(`/recipes/${req.body.recipeId}/ingredients/new`)
+    
+})
+
+// GET /recipes/edit/:id to show a form that will edit a recipe
+router.get('/edit/:id', async (req,res) => {
+    const recipe = await db.recipe.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    // show an edit form for the recipe
+    res.render('recipes/edit', {recipe: recipe})
+})
+
+// GET /recipes/:id/ingredients/edit/:id to show a form for editing an ingredient 
+
+router.get('/:recipeId/ingredients/edit/:ingrId', async(req,res) => {
+    const recipe = await res.locals.user.getRecipes({
+        where: {
+            id: req.params.recipeId
+        }
+    })
+    // console.log(recipe[0], 'hello ther')
+    const ingredient = await recipe[0].getIngredients({
+        where: {
+            id: req.params.ingrId
+        }
+    })
+    // console.log(ingredient,'🥶🥶')
+    res.render('recipes/ingredients/edit', {recipe:recipe[0], ingredient: ingredient[0]})
+})
+
+// PUT /recipes/:id/ingredients/:id to make changes to an ingredient 
+router.put('/:recipeId/ingredients/:ingredientId', async (req,res) => {
+    try {
+        const ingredient = await db.ingredient.findOne({
+            where: {
+                id: req.params.ingredientId
+            }
+        })
+
+        ingredient.name = req.body.ingrName
+        ingredient.measure = req.body.ingrMeasure
+        ingredient.quantity = req.body.ingrQuantity
+        await ingredient.save()
+        console.log(ingredient)
+        const recipeId = ingredient.recipeId
+        res.redirect(`/recipes/${recipeId}/ingredients/new`)
 
     }
     catch (err) {
@@ -73,37 +106,7 @@ router.post('/', async (req,res) => {
     }
 })
 
-// POST /recipes/:id to add ingredients to the recipe 
-router.post('/:id', async (req,res) => {
-    const newIngredient = await db.ingredient.create({
-        name: req.body.ingrName, 
-        measure: req.body.ingrMeasure,
-        quantity: req.body.ingrQuantity, 
-        recipeId: req.params.id
-    })
-    console.log(newIngredient)
-    res.redirect(`/recipes/${req.params.id}`)
-    
-})
-
-// PUT /recipes/:id to update the recipe details and ingredients
-
-router.put('/:id', async (req,res) => {
-    // console.log('Help Me😳😳😳😳😳😳😳😳')
-    const recipe = await db.recipe.findOne({
-        where: {
-            id: req.params.id
-        }
-    })
-    // console.log('this is the RECIPE NAME 🔥🔥🔥🔥🔥🔥', recipe.name)
-    recipe.directions = req.body.directions  
-    recipe.story = req.body.story
-    recipe.notes = req.body.notes
-    await recipe.save()
-    res.redirect(`/recipes/${recipe.id}`)
-})
 // GET /recipes/:id show details about specific recipe, grab associated ingredients 
-
 router.get('/:id', async (req,res) => {
    try {
     if (!res.locals.user) {
@@ -143,8 +146,33 @@ router.get('/:id', async (req,res) => {
    }
 })
 
+// PUT /recipes/:id to edit the recipe details
+router.put('/:id', async (req,res) => {
+    // console.log('Help Me😳😳😳😳😳😳😳😳')
+    const recipe = await db.recipe.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    // console.log('this is the RECIPE NAME 🔥🔥🔥🔥🔥🔥', recipe.name)
+    recipe.directions = req.body.directions  
+    recipe.story = req.body.story
+    recipe.notes = req.body.notes
+    await recipe.save()
+    res.redirect(`/recipes/${recipe.id}`)
+})
+
+// GET Route to show Form to add ingredients to a given recipe
 router.get('/:recipeId/ingredients/new', async (req, res) => {
-    res.render('recipes/ingredients/new', {recipeId: req.params.recipeId})
+    const ingredients = await db.ingredient.findAll({
+        where: {
+            recipeId: req.params.recipeId
+        }
+    })
+    res.render('recipes/ingredients/new', {
+        recipeId: req.params.recipeId, 
+        ingredients: ingredients
+    })
 })
 
 
