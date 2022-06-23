0285190bac6df446ac9c58d35308e43abf451c1f
mitchdmarino <mitchdmarino@gmail.com>
2022-06-23 00:46:37 -0700

    Works on basic styling / categories CRUD / associations between categories and recipes

---
+++ Public/javascript/navbar.js
@@ -0,0 +1,31 @@
+// // Source: https://flowbite.com/docs/components/navbar/
+
+// // target profile dropdown 
+// const profileDropDown = document.getElementById('dropdown')
+
+// // triger: user Element button 
+// const profileTrigger = document.getElementById('user-menu-button')
+
+// const options = {
+//     profileTrigger: profileTrigger, 
+//     onCollapse: () => {
+//         console.log('element has been collapsed')
+//     },
+//     onExpand: () => {
+//         console.log('element has been expanded')
+//     },
+//     onToggle: () => {
+//         console.log('element has been toggled')
+//     }
+// }
+
+// const collapse = new Collapse(profileDropDown, options)
+
+
+
+
+
+
+
+// target mobile dropdown 
+// const mobileDropDown = document.getElementsById('')
